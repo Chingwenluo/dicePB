e7d8e2b55131638074e129e8d26a3993eb0017a3
ChingWen <fsvnxwx@gmail.com>
2020-09-22 23:53:30 +0800

    Initial Commit

---
+++ dicePB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dicePB
+//
+//  Created by eric on 2020/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
